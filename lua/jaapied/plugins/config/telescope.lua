6d355891e3d454433394cf477c4de39847935555
Jacob Degeling <jacobdegeling@redbendcc.nsw.edu.au>
2026-08-31 10:53:53 +1000

    fix(telescope): add fzf-native for correct match ranking
    Without telescope-fzf-native, the default Lua sorter doesn't weight
    path proximity, so a nested init.vim could outrank ./init.vim.

--- lua/jaapied/plugins/config/telescope.lua
+++ lua/jaapied/plugins/config/telescope.lua
@@ -1,7 +1,10 @@
 return {
   'nvim-telescope/telescope.nvim',
   -- tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
   config = function()
     require('telescope').setup({
       pickers = {
@@ -16,7 +19,16 @@ return {
           hidden = true
         }
       },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        }
+      },
     })
+    require('telescope').load_extension('fzf')
     local keymap_opts = { noremap = true, silent = true }
     vim.keymap.set('n', '<C-p>', ':Telescope find_files<cr>', keymap_opts)
     vim.keymap.set('n', '<C-b>', ':Telescope buffers<cr>', keymap_opts)
